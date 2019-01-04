dc12645bdd94e2e93ad1dd3339d124f11c9d2d10
jaymeramsay <jayme.ramsay@gmail.com>
2019-01-04 13:18:15 -0600

    imports normalize and updates scss file path

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './App.scss'
 import PixelArtMaker from './components/PixelArtMaker';
-
+import 'normalize.css/normalize.css';
+import './styles/App.scss';
 
 ReactDOM.render(<PixelArtMaker />, document.getElementById('app'));
