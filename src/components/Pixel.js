1f2e39f3f0851af56c9b668ad6fd27bc3e02c71e
jaymeramsay <jayme.ramsay@gmail.com>
2019-01-08 12:57:44 -0600

    adds classname to div

--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -4,7 +4,7 @@ export default class Pixel extends React.Component {
     render() {
         return (
             <div>
-                <div>p</div>
+                <div className="pixel"></div>
             </div>
         )
     }
