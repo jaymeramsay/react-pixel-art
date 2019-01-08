85c7b811dce28d91e21b5bcf7b9e17d5aa67ffa8
jaymeramsay <jayme.ramsay@gmail.com>
2019-01-08 12:59:28 -0600

    refactors divs and adds classname

--- src/components/PixelBoard.js
+++ src/components/PixelBoard.js
@@ -5,19 +5,15 @@ import Pixel from './Pixel';
 const PixelBoard = (props) => {
     console.log(props);
    return (
-        <div>
-        This is my pixel art board yay!
-            <div>
+            <div className="pixelboard">
                 {
                     props.pixels.map((pixel) => (
                         <Pixel 
                             key={pixel}
-                            className='pixel'
                         />
                     ))
                 }
             </div>
-        </div>
     )
 }
 
